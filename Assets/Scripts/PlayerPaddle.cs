4742de0edddd97f6cc241f5b563a40e9f1df339f
Bryce Wendelaar <bryce@brycewendelaar.com>
2021-08-02 11:37:51 -0400

    C# script setup

--- Assets/Scripts/PlayerPaddle.cs
+++ Assets/Scripts/PlayerPaddle.cs
@@ -1,18 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerPaddle : MonoBehaviour
+public class PlayerPaddle : Paddle
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
