3a5470e21a1516f49c01ac9156cf5034f5d97f83
Bryce Wendelaar <bryce@brycewendelaar.com>
2021-08-02 14:10:35 -0400

    Set up game manager

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -13,13 +13,26 @@ public class Ball : MonoBehaviour
     }
     private void Start()
     {
+        ResetPosition();
+    }
+
+    public void ResetPosition()
+    {
+        _rigidbody.position = Vector3.zero;    
+        _rigidbody.velocity = Vector3.zero;
+
         AddStartingForce();
     }
+
     private void AddStartingForce() 
     {
         float x = Random.value < 0.5f ? -1.0f : 1.0f;
         float y = Random.value < 0.5f ? Random.Range(-1.0f, -0.5f) : Random.Range(0.5f, 1.0f);
         Vector2 direction = new Vector2(x, y);
         _rigidbody.AddForce(direction * this.speed);
+    }
+    public void AddForce(Vector2 force)
+    {
+        _rigidbody.AddForce(force);
     }
 }
