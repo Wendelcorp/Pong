2ebc62e38a22d0c284c72f14c25f3fe1d6bd3c01
Bryce Wendelaar <bryce@brycewendelaar.com>
2021-08-02 14:25:49 -0400

    Updated score tracking system

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -11,11 +11,13 @@ public class GameManager : MonoBehaviour
     public void PlayerScores()
     {
         _playerScore++;
+        Debug.Log(_playerScore);
         this.ball.ResetPosition();
     }
     public void ComputerScores()
     {
         _computerScore++;
+        Debug.Log(_computerScore);
         this.ball.ResetPosition();
     }
 
